26266f7c2616f89d27a2b46593c237a733756aaf
Andrei Mulev <a.mulev.vironit@vironit.ru>
2020-01-22 11:29:49 +0300

    Added finished Component and repeat button

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import classes from './Quiz.module.scss';
 import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz';
+import FinishedQuiz from '../../components/FinishedQuiz/FinishedQuiz';
 
 class Quiz extends Component {
 	state = {
+		results: {},
 		isFinished: false,
 		activeQuestion: 0,
 		answerState: null,
@@ -40,9 +42,16 @@ class Quiz extends Component {
 		}
 		const question = this.state.quiz[this.state.activeQuestion];
 
+		const results = this.state.results;
+
 		if (question.reightAnswerId === answerId) {
+			if (!results[question.id]) {
+				results[question.id] = 'success';
+			}
+
 			this.setState({
 				answerState: { [answerId]: 'success' },
+				results,
 			});
 
 			const timeOut = window.setTimeout(() => {
@@ -59,8 +68,10 @@ class Quiz extends Component {
 				window.clearTimeout(timeOut);
 			}, 1000);
 		} else {
+			results[question.id] = 'error';
 			this.setState({
 				answerState: { [answerId]: 'error' },
+				results,
 			});
 		}
 	};
@@ -68,6 +79,15 @@ class Quiz extends Component {
 	isQuizFinished() {
 		return this.state.activeQuestion + 1 === this.state.quiz.length;
 	}
+
+	retryHandler = () => {
+		this.setState({
+			activeQuestion: 0,
+			answerState: null,
+			isFinished: false,
+			results: {},
+		});
+	};
 	render() {
 		return (
 			<div className={classes.Quiz}>
@@ -75,7 +95,11 @@ class Quiz extends Component {
 					<h1>Ответьте на все вопросы</h1>
 
 					{this.state.isFinished ? (
-						<h1>Finished</h1>
+						<FinishedQuiz
+							results={this.state.results}
+							quiz={this.state.quiz}
+							onRetry={this.retryHandler}
+						/>
 					) : (
 						<ActiveQuiz
 							answers={
